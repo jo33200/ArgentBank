24fe089179ec089c0402f120897d8495056d979c
jo33200 <jo33200@gmail.com>
2024-06-05 16:19:56 +0200

    build: create routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-      <Route path="/" component={Home} />
+      <Navbar />
+      <Routes>
+        <Route path="/" component={Home} />
+      </Routes>
     </BrowserRouter>
   );
 }
