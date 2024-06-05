e7ccf475f1ad9afc6710918935d42ad6c4cb6139
jo33200 <jo33200@gmail.com>
2024-06-05 16:21:22 +0200

    fix: first navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import './Navbar.css'
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/argentBankLogo.webp'
 
 const Navbar = () => { 
+
     return (
-        <nav className="navbar">
-            <p>Navbar</p>
+        <nav id="nav">
+            <NavLink to="/">
+                <img src={Logo} className="main-nav-logo-image" alt="Argent Bank Logo"/>
+                <h1 className="sr-only">Argent Bank</h1>
+            </NavLink>
+            <div>
+                <a className="main-nav-item" href="./sign-in.html">
+                <i className="fa fa-user-circle"></i>
+                Sign In
+                </a>
+            </div>
         </nav>
     );
 };
