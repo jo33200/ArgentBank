9fdf7be2fd3ac841637219218440b7c3aefda02c
jo33200 <jo33200@gmail.com>
2024-06-05 16:44:36 +0200

    fix: navlink ?

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,12 +9,14 @@ const Navbar = () => {
             <NavLink to="/">
                 <img src={Logo} className="main-nav-logo-image" alt="Argent Bank Logo"/>
             </NavLink>
+            <NavLink to="/">
             <div>
                 <a className="main-nav-item" href="./sign-in.html">
                 <i className="fa fa-user-circle"></i>
                 Sign In
                 </a>
             </div>
+            </NavLink>
         </nav>
     );
 };
