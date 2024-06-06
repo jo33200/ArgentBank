cc61a5ccce53a4c78522ae9d8744728cef594879
jo33200 <jo33200@gmail.com>
2024-06-06 15:48:25 +0200

    build: new path user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/home/home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Login from './pages/login/login';
+import User from './pages/user/user';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
