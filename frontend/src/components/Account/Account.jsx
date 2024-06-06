7c39f2ef45c28f4614b5651110f78faf4f5de36f
jo33200 <jo33200@gmail.com>
2024-06-06 15:56:47 +0200

    build: account component

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -1,11 +1,27 @@
-import './Account.css';
+import './Account.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button/Button';
 
-const Account = () => {
+const Account = (title, amount, description) => {
     return (
-        <div className="account">
-            <p>Account</p>
+        <>
+        <div class="account-content-wrapper">
+          <h3 class="account-title">{title}</h3>
+          <p class="account-amount">{amount}</p>
+          <p class="account-amount-description">{description}</p>
         </div>
+        <Button to="/*" >
+        View transactions
+        </Button>
+        </>
     );
+}
+
+    Account.propTypes = {
+        title: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
 };
 
 export default Account;
