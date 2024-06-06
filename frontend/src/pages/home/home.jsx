08deff8d4d495ac43b6044b12491cfd3d4ca421b
jo33200 <jo33200@gmail.com>
2024-06-06 09:50:54 +0200

    fix: homepage static ok

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,16 +1,42 @@
 import './home.css';
+import React from 'react';
+import Features from '../../components/Features/Features';
+
+import chatIcon from '../../assets/icon-chat.png';
+import moneyIcon from '../../assets/icon-money.png';
+import securityIcon from '../../assets/icon-security.png';
 
 const Home = () => {
     return (
+        <>
         <div class="hero">
-            <section class="hero-content">
-            <h2 class="sr-only">Promoted Content</h2>
-            <p class="subtitle">No fees.</p>
-            <p class="subtitle">No minimum deposit.</p>
-            <p class="subtitle">High interest rates.</p>
-            <p class="text">Open a savings account with Argent Bank today!</p>
+            <section className="hero-content">
+            <h2 className="sr-only">Promoted Content</h2>
+            <p className="subtitle">No fees.</p>
+            <p className="subtitle">No minimum deposit.</p>
+            <p className="subtitle">High interest rates.</p>
+            <p className="text">Open a savings account with Argent Bank today!</p>
             </section>
-        </div>
+        </div> 
+        <section className="features">
+            <Features
+            icon={chatIcon} 
+            title="You are our #1 priority"
+            description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."/>
+             <Features 
+                icon={moneyIcon}
+                title="More savings means higher rates"
+                description="The more you save with us, the higher your interest rate will be!"
+            />
+            <Features 
+                icon={securityIcon}
+                title="Security you can trust"
+                description="We use top of the line encryption to make sure your data and money is always safe."
+            />
+        </section>
+        </>
+        
+        
     );
 };
 
