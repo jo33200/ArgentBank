2ad7b99cdd20d09a5b00d97e627471f0922d5805
jo33200 <jo33200@gmail.com>
2024-06-07 17:43:18 +0200

    fix: height page

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -8,35 +8,33 @@ import securityIcon from '../../assets/icon-security.png';
 
 const Home = () => {
     return (
-        <>
-        <div class="hero">
-            <section className="hero-content">
-            <h2 className="sr-only">Promoted Content</h2>
-            <p className="subtitle">No fees.</p>
-            <p className="subtitle">No minimum deposit.</p>
-            <p className="subtitle">High interest rates.</p>
-            <p className="text">Open a savings account with Argent Bank today!</p>
+        <div className='main'>
+            <div class="hero">
+                <section className="hero-content">
+                <h2 className="sr-only">Promoted Content</h2>
+                <p className="subtitle">No fees.</p>
+                <p className="subtitle">No minimum deposit.</p>
+                <p className="subtitle">High interest rates.</p>
+                <p className="text">Open a savings account with Argent Bank today!</p>
+                </section>
+            </div> 
+            <section className="features">
+                <Features
+                icon={chatIcon} 
+                title="You are our #1 priority"
+                description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."/>
+                <Features 
+                    icon={moneyIcon}
+                    title="More savings means higher rates"
+                    description="The more you save with us, the higher your interest rate will be!"
+                />
+                <Features 
+                    icon={securityIcon}
+                    title="Security you can trust"
+                    description="We use top of the line encryption to make sure your data and money is always safe."
+                />
             </section>
         </div> 
-        <section className="features">
-            <Features
-            icon={chatIcon} 
-            title="You are our #1 priority"
-            description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."/>
-             <Features 
-                icon={moneyIcon}
-                title="More savings means higher rates"
-                description="The more you save with us, the higher your interest rate will be!"
-            />
-            <Features 
-                icon={securityIcon}
-                title="Security you can trust"
-                description="We use top of the line encryption to make sure your data and money is always safe."
-            />
-        </section>
-        </>
-        
-        
     );
 };
 
--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -7,24 +7,26 @@ const Login = () => {
 
   return (
     <main className="main bg-dark">
+      <div className="container-sign-in">
        <section className="sign-in-content">
-        <i className="fa fa-user-circle "></i>
-        <h1>Sign In</h1>
-        <form>
-          <div className="input-wrapper">
-            <label htmlFor="username">Username</label><input type="text" id="username" />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="password">Password</label><input type="password" id="password" />
-          </div>
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
-          </div>
-          <Button className="button-sign" to="/user" >
-        Sign In
-      </Button>
-        </form>
-      </section>
+          <i className="fa fa-user-circle "></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label><input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label><input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
+            </div>
+            <Button className="button-sign" to="/user" >
+              Sign In
+            </Button>
+          </form>
+        </section>  
+      </div>
     </main>
   );
 };
