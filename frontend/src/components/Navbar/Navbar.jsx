be7388c67a88354b7db832e722987400046d42b5
jo33200 <jo33200@gmail.com>
2024-06-13 11:17:55 +0200

    fix: navbar test

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,48 @@
-import './Navbar.scss'
-import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/argentBankLogo.webp'
+import './Navbar.scss';
+import { NavLink, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Logo from '../../assets/argentBankLogo.webp';
 import React from 'react';
+import { Logout } from '../../redux/logout/logout';
 
-const Navbar = () => { 
+const Navbar = () => {
+  const user = useSelector((state) => state.userLogin.user);
+  const dispatch = useDispatch();
+  const authentification = useSelector((state) => state.auth.authentification);
 
-    return (
-        <nav id="nav">
-            <NavLink to="/">
-                <img src={Logo} className="main-nav-logo-image" alt="Argent Bank Logo"/>
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(Logout());
+    Navigate('/login');
+
+  };
+
+  return (
+    <nav id="nav">
+      <NavLink to="/">
+        <img src={Logo} className="main-nav-logo-image" alt="Argent Bank Logo" />
+      </NavLink>
+      {authentification ? (
+          <>
+            <NavLink to='/profile'>
+                <i className='fa fa-user-circle main-nav-item'></i>
+                {user?.userName}
             </NavLink>
-            <div>
-                <NavLink to="/login" className="main-nav-item">
-                <i className="fa fa-user-circle"></i>
-                    <p>Sign In</p>
-                </NavLink>
-            </div>
-        </nav>
-    );
+            <NavLink to='/' onClick={handleLogout}>
+                <i className='fa fa-sign-out'></i>
+                Sign Out
+            </NavLink>
+          </>
+      ) : (
+        <div>
+          <NavLink to="/login" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            <p>Sign In</p>
+          </NavLink>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
