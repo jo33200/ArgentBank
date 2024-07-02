a11c8e4cf37b941cb0238c305f9f95b7cb29ee52
jo33200 <jo33200@gmail.com>
2024-07-02 12:15:34 +0200

    feat: Add protected routes for login and dashboard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer/Footer';
 import Login from './pages/login/login';
 import Dashboard from './pages/dashboard/dashboard';
 import Error from './pages/error/error';
+import ProtectedRoute from './protectedRoute';
 
 function App() {
   return (
@@ -13,8 +14,16 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={ 
+          <ProtectedRoute isProtected={false}>
+            <Login />
+          </ProtectedRoute>} 
+        />
+        <Route path="/dashboard" element={ 
+          <ProtectedRoute isProtected={true}>
+              <Dashboard />
+          </ProtectedRoute>} 
+        />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer/>
