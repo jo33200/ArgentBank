011d44ac13cafdcf5437af0e6145ecc0271b073f
jo33200 <jo33200@gmail.com>
2024-07-15 18:20:30 +0200

    chore: Update Redux store configuration and persisting for authentication

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,14 +3,12 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
 import authSlice from './reducers/auth.reducers'; 
 
-
 const persistConfig = {
   key: 'root', 
   storage, 
 };
 
 const persistedReducer = persistReducer(persistConfig, authSlice);
-
 
 export const store = configureStore({
   reducer: {
